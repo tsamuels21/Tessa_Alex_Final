7f2db11738a6d08159a82caa7f4c388208d37473
tessasamuels <tsamuels@fwparker.org>
2019-05-13 09:02:16 -0500

    plan

---
+++ plan.py
@@ -0,0 +1,35 @@
+'''
+Step 1:
+Shuffling card deck images
+    in console not displaying images
+
+Step 2:
+get shuffled deck to appear on screen
+    random card appear on screen
+
+Step 3:
+click to make new card appear from shuffled deck
+
+Step 4:
+split deck into 2 randomly
+
+Step 5:
+select random from each deck, then compare, find higher card
+    in console
+
+Step 6:
+comparing on screen
+
+Step 7:
+have cards go to winner + display # of cards
+    console
+
+Step 8:
+have cards go to winner + display # of cards
+    in kivy
+
+Step 9:
+keep playing till all the cards are gone
+    moving around lists
+
+'''
